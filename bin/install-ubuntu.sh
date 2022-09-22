7c5c24e17d28e1e82c27de93aac70fb1e12948e5
niyialimiw <niyi.alimi@contino.io>
2022-09-22 23:40:02 +1000

    test: pre-commit hook fixed

--- bin/install-ubuntu.sh
+++ bin/install-ubuntu.sh
@@ -15,4 +15,3 @@ pre-commit install
 echo 'setting pre-commit hooks to auto-install on clone in the future'
 git config --global init.templateDir ~/.git-template
 pre-commit init-templatedir ~/.git-template
-
