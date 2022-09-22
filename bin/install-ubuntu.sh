1ca0e933f756a0433e31b6c98878ccc9142a14c4
niyialimiw <niyi.alimi@contino.io>
2022-09-22 23:35:16 +1000

    test: pre-commit hook fixed

--- bin/install-ubuntu.sh
+++ bin/install-ubuntu.sh
@@ -7,10 +7,7 @@ tfdocs_latest_dl_url=$(curl -sL https://api.github.com/repos/terraform-docs/terr
 curl -L "$tfdocs_latest_dl_url" > terraform-docs && chmod +x terraform-docs && sudo mv terraform-docs /usr/bin/
 curl -L "$(curl -sL https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip && sudo mv tflint /usr/bin/
 env GO111MODULE=on go get -u github.com/liamg/tfsec/cmd/tfsec
-git clone https://github.com/tfutils/tfenv.git ~/.tfenv || true
-mkdir -p ~/.local/bin/
-. ~/.profile
-ln -s ~/.tfenv/bin/* ~/.local/bin
+
 
 echo 'installing pre-commit hooks'
 pre-commit install
@@ -19,5 +16,3 @@ echo 'setting pre-commit hooks to auto-install on clone in the future'
 git config --global init.templateDir ~/.git-template
 pre-commit init-templatedir ~/.git-template
 
-echo 'installing terraform with tfenv'
-tfenv install latest
